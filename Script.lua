a30df9e2654d6f72bf2fd1358a68800197e34cc5
Fylwind <fyl@wolfpa.ws>
2026-01-20 02:50:49 -0800

    Create initial stub

---
+++ Script.lua
@@ -0,0 +1,10 @@
+FylMovementSpeedMixin = {}
+
+local function movementSpeed()
+    local isGliding, _, forwardSpeed = C_PlayerInfo.GetGlidingInfo()
+    return string.format("%d", ((isGliding and forwardSpeed or GetUnitSpeed("Player")) / BASE_MOVEMENT_SPEED) * 100)
+end
+
+function FylMovementSpeedMixin:OnLoad()
+    self.Text:SetText(format("mvspd=%d%%", movementSpeed()))
+end
